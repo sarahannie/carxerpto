5d78e0c23081d65613268ad8c51ba82f79dc9ca4
sarahannie <sarahndianekwu@gmail.com>
2024-04-09 16:43:01 +0100

    profile flow done

--- src/components/stripe/stripe.js
+++ src/components/stripe/stripe.js
@@ -5,8 +5,8 @@ import CheckoutForm from './checkoutform';
 // Make sure to call `loadStripe` outside of a component’s render to avoid
 // recreating the `Stripe` object on every render.
 const stripePromise = loadStripe('pk_test_qblFNYngBkEdjEZ16jxxoWSM');
-const secret ='sk_test_26PHem9AhJZvU623DfE1x4sd'
-const id = 'ca_12345'
+// const secret ='sk_test_26PHem9AhJZvU623DfE1x4sd'
+// const id = 'ca_12345'
 export default function Stripe() {
   const options = {
     // passing the client secret obtained from the server
