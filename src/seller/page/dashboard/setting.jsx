8f25be502b59fbe37dc888ecbef1605eb6cba844
sarahannie <sarahndianekwu@gmail.com>
2024-04-12 17:18:32 +0100

    seller setting page done

--- src/seller/page/dashboard/setting.jsx
+++ src/seller/page/dashboard/setting.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import DashboardNavbar from "../../widgets/layout/dashboard-navbar";
 import Sidenav from "../../widgets/layout/sidenav";
+import Profilebody from "../../layouts/profileCon/Profilebody";
 
 
 export function Setting() {
@@ -12,10 +13,8 @@ export function Setting() {
         <div className="">
         <Sidenav/>
         </div>
-        <div className=" mt-7 w-[90%] lg:ml-[20%] ml-[15px]">
-            
-        
-        
+        <div className=" mt-7 w-[90%] lg:ml-[20%] ml-[15px]">   
+        <Profilebody />
         </div>
     </div>
       
