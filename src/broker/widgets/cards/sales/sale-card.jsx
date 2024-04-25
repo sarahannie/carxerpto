0fa6657c3f4a2e9df525e590f245b5fc1b25965c
sarahannie <sarahndianekwu@gmail.com>
2024-04-25 05:51:01 +0100

    small screen for broker done

--- src/broker/widgets/cards/sales/sale-card.jsx
+++ src/broker/widgets/cards/sales/sale-card.jsx
@@ -1,4 +1,3 @@
-
 import { IoBagHandleOutline } from "react-icons/io5";
 import { TiEdit } from "react-icons/ti";
 import { Link } from "react-router-dom";
@@ -8,52 +7,49 @@ import { MdOutlineReviews } from "react-icons/md";
   
 export function HomeCard({ color, icon, title, value, footer }) {
   return (
-   <div className="border border-primary-normal pt-4 pb-1 px-3 rounded-md shadow-lg ">
+    <div className="border border-primary-normal pt-4 pb-1 px-3 rounded-md shadow-lg">
       <div className="flex justify-between py-3 align-center text-primary-normal">
+        <div>
+          <p className="p"></p>
+          <h3>Jammie Chan</h3>
+        </div>
+        <div className="">
+          <Link to="/dashboard/brokerprofileedit">
+            <div className="flex gap-2 text-primary-normal"> 
+              Edit profile <TiEdit className="mt-1" />
+            </div>
+          </Link>
+        </div>
+      </div>
+      <div className="mb-4 grid gap-y-6 grid-cols-2 md:grid-cols-3 text-primary-normal">
+        <div className="flex gap-2 items-center">
           <div>
-              <p className="p"></p>
-              <h3>Jammie Chan</h3>
+            <PiCertificateThin fontSize={30} className="mt-2" />
           </div>
-          <div className="" >
-            <Link to="/dashboard/brokerprofileedit">
-                <div className="flex gap-2 text-primary-normal"> 
-                Edit profile <TiEdit className="mt-1" /></div>
-                </Link>
+          <div className="text-start">
+            <h3 className="text-gray-400 text-[13px]">Sales</h3>
+            <p className="text-black-900 text-lg">$25,000</p>
           </div>
+        </div>
+        <div className="flex gap-2 items-center">
+          <div>
+            <TfiCup fontSize={27} className="mt-2" />
+          </div>
+          <div className="text-start">
+            <h3 className="text-gray-400 text-[13px]">Success rate</h3>
+            <p className="text-black-900 text-lg">80%</p>
+          </div>
+        </div>
+        <div className="flex gap-2 items-center">
+          <div>
+            <MdOutlineReviews fontSize={27} className="mt-2" />
+          </div>
+          <div className="text-start">
+            <h3 className="text-gray-400 text-[13px]">Review</h3>
+            <p className="text-black-900 text-lg">20+</p>
+          </div>
+        </div>
       </div>
-      <div className=" mb-6 grid gap-y-10 gap-x-6 grid-cols-3 mt-5 text-primary-normal">
-        <div className="flex gap-2">
-            <div>
-                <PiCertificateThin fontSize={37} className="mt-2"/>
-            </div>
-            <div className="text-start ">
-                <h3 className="text-gray-400 text-[13px]">Sales</h3>
-                <p className="text-black-900 text-xl ">$25,000</p>
-            </div>
-        </div>
-        <div className="flex gap-2">
-            <div>
-                <TfiCup fontSize={27} className="mt-2"/>
-            </div>
-            <div className="text-start ">
-                <h3 className="text-gray-400 text-[13px]">Success rate</h3>
-                <p className="text-black-900 text-xl ">80%</p>
-            </div>
-        </div>
-        <div className="flex gap-2">
-            <div>
-                <MdOutlineReviews fontSize={27} className="mt-2"/>
-            </div>
-            <div className="text-start ">
-                <h3 className="text-gray-400 text-[13px]">Review</h3>
-                <p className="text-black-900 text-xl ">20+</p>
-            </div>
-        </div>
-          
-        
-          
-          
-      </div>
-   </div>
+    </div>
   );
 }
--- src/broker/widgets/cards/sales/sale-card1.jsx
+++ src/broker/widgets/cards/sales/sale-card1.jsx
@@ -1,33 +1,22 @@
-
 import { BsFillPeopleFill } from "react-icons/bs";
 
-  
 export function HomeCard1({ color, icon, title, value, footer }) {
   return (
-   <div className="border border-primary-normal pt-4 pb-1 px-3 text-white bg-primary-normal rounded-md shadow-lg">
+    <div className="border border-primary-normal pt-4 pb-1 px-3 text-white bg-primary-normal rounded-md shadow-lg">
       <div className="flex justify-between py-3 align-center text-white ">
-          <div>
-              Contact Information
-          </div>
-          <div className="" >
-         
-          </div>
+        <div>Contact Information</div>
+        <div className=""></div>
       </div>
-      <div className=" mb-6 grid gap-y-10 gap-x-6 grid-cols-3 mt-5 text-primary-normal">
-          <div className="text-start ">
-              <h3 className="text-gray-400">Email</h3>
-              <p className="text-white text-[13px]">Janenetsmith@gmail.com</p>
-          </div>
-          <div className="text-start ">
-              <h3 className="text-gray-400">Phone</h3>
-              <p className="text-white text-[13px]">+123 456789</p>
-          </div>
-          <div className="text-start ">
-              <h3 className="text-gray-400 text-bold">Location</h3>
-              <p className="text-white text-[13px]">San Jose, USA</p>
-          </div>
-          
+      <div className="mb-4 grid gap-3 grid-cols-2 md:grid-cols-2 text-primary-normal">
+        <div className="text-start ">
+          <h3 className="text-gray-400">Email</h3>
+          <p className="text-white text-sm">Janenetsmith@gmail.com</p>
+        </div>
+        <div className="text-start ">
+          <h3 className="text-gray-400 text-bold">Location</h3>
+          <p className="text-white text-sm">San Jose, USA</p>
+        </div>
       </div>
-   </div>
+    </div>
   );
 }
