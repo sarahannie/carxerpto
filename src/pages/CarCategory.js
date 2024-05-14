614afaf0b1baab8123b472c8cdcfbef67545707a
sarahannie <sarahndianekwu@gmail.com>
2024-05-14 03:44:02 +0100

    adjusted the category page

--- src/pages/CarCategory.js
+++ src/pages/CarCategory.js
@@ -11,13 +11,13 @@ function CarCategory() {
     <div>
       <section className="w-full h-full overflow-x-hidden">
         <section>
-          <div className="relative">
+          <div className="w-full h-[500px]">
             <img
               src={Suv}
               alt="car"
-              className="inset-0 h-[550px] w-full object-cover"
+              className="w-full h-full absolute lg:object-cover object-bottom mix-blend-overlay"
             />
-            <div className="top-40 absolute lg:top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white">
+            <div className="top-40 absolute lg:top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white pb-24">
               <h1 className="font-bold text-2xl md:text-4xl lg:text-2xl mb-2">Categories</h1>
               <p className="text-sm md:text-base lg:text-4xl font-bold mb-2">Find the Perfect SUV for your Lifestyle</p>
               <p className="text-sm md:text-base lg:text-lg font-semibold">Explore SUVs for your next Adventure</p>
